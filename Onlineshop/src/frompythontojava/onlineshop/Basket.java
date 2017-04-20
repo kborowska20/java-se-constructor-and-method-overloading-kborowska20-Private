871e43ca89508716b4977fbd63f478e58f4780b7
klaudia borowska <canimi18@gmial.com>
2017-04-20 16:45:19 +0200

    create Basket class

--- Onlineshop/src/frompythontojava/onlineshop/Basket.java
+++ Onlineshop/src/frompythontojava/onlineshop/Basket.java
@@ -1,3 +1,29 @@
-class Bakset {
+import java.util.Iterator;
+import java.util.ArrayList;
 
+
+class Basket {
+    private ArrayList<Product> productList;
+    private Iterator iterator;
+
+    public Basket() {
+        this.iterator = iterator;
+        this.productList = new ArrayList<Product>();
+
+    }
+
+    public Iterator getIterator(){ return this.iterator;}
+
+    public void addProduct(Product product){
+        this.productList.add(product);
+    }
+
+    public Boolean removeProduct(Product product){
+         if(this.productList.remove(product)) {
+            return true;
+        } else {
+             return false;
+        }
+
+    }
 }
